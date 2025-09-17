6bdb0f49a0b0c1e9665b6719cfbce250c9835198
Lewis-Dev1 <kawardin5@gmail.com>
2025-09-18 01:35:27 +0300

    Added some navigational context and a middleware file

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Link } from 'expo-router';
 
-const Home = () => {
+type Props = {};
+
+const Home = (props: Props) => {
   return (
-    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <SafeAreaView
+      style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+    >
       <View>
         <Text>Home</Text>
+        <Link href='/signup' asChild>
+          <TouchableOpacity>
+            <Text>Sign Up</Text>
+          </TouchableOpacity>
+        </Link>
+        <Link href='/login' asChild>
+          <TouchableOpacity>
+            <Text>Login</Text>
+          </TouchableOpacity>
+        </Link>
       </View>
     </SafeAreaView>
   );
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,14 +2,31 @@ import { Stack } from 'expo-router';
 import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import '@/global.css';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+
+const [loaded] = useFonts({
+  'Inter-Regular': require('@/assets/fonts/Inter-Regular.ttf'),
+});
+
+useEffect(() => {
+  if (loaded) {
+    SplashScreen.hideAsync();
+  }
+}, [loaded]);
 
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
-      <GluestackUIProvider mode="light">
+      <GluestackUIProvider mode='light'>
         <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="(tabs)" />
-          <Stack.Screen name="(screens)" />
+          <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+          <Stack.Screen name='(screens)' />
+          <Stack.Screen name='signup' options={{ presentation: 'modal' }} />
+          <Stack.Screen name='login' options={{ presentation: 'modal' }} />
         </Stack>
       </GluestackUIProvider>
     </SafeAreaProvider>
