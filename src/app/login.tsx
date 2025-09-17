fdefe0bdf3a355f247b31106d9bd36c73c53e324
Lewis-Dev1 <kawardin5@gmail.com>
2025-09-18 01:42:02 +0300

    Updating the expo packages, and setting up basic navigation to test if it works

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { Link } from 'expo-router';
 
 const Login = () => {
   return (
     <View>
-      <Text>Login</Text>
+      <View className='justify-center items-center font-bold text-2xl'>
+        <Text> Login Screen</Text>
+      </View>
+      <Link href='/home' asChild>
+        <TouchableOpacity>
+          <Text>Back to Home</Text>
+        </TouchableOpacity>
+      </Link>
     </View>
   );
 };
