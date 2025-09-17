79f850024b938bbd0569b95396fdf18039d36341
Lewis-Dev1 <kawardin5@gmail.com>
2025-09-17 23:43:23 +0300

    Created color constants for the App UI, set up a supabase client for authentication, created different types, installed extra packages for linking and videos

---
+++ src/constants/constant.ts
@@ -0,0 +1,9 @@
+export const Colors = {
+  primary: '#0341fc',
+  black: '#333',
+  grey: '#666',
+  lightgrey: '#999',
+  white: '#fff',
+  background: '#F4F4F4',
+  highlight: '#F4CE14',
+};
--- src/types/types.ts
+++ src/types/types.ts
@@ -25,3 +25,32 @@ export interface User {
   updatedAt: Date;
   deletedAt: Date;
 }
+export interface Category {
+  id: string;
+  name: string;
+  icon: string;
+}
+export interface SubCategory {
+  id: string;
+  name: string;
+  icon: string;
+  parentCategory: Category;
+}
+export interface ListingCard {
+  id: string;
+  title: string;
+  description: string;
+  price: number;
+  location: string | number[];
+  rating: number;
+  reviews: Review[];
+  seller: User;
+}
+export interface Plan {
+  id: string;
+  name: string;
+  price: number;
+  features: string[];
+  duration: number;
+  maxListings: number;
+}
