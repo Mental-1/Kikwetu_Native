fd13a6417e5661a255201d2ced58ebc4462d82e7
Mental-1 <njengalewis133@gmail.com>
2025-11-18 17:03:40 +0300

    updating metro

--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,8 @@ const {
   getSentryExpoConfig
 } = require("@sentry/react-native/metro");
 
-const config = getSentryExpoConfig(__dirname);
+// Get the Sentry config. 
+// We use `.` instead of `__dirname` to work around a Metro issue on Windows.
+const config = getSentryExpoConfig('.');
 
 module.exports = withNativeWind(config, { input: './global.css' });
