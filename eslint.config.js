d988f4fef5337b0769db82fcd842e88c178bbac9
Mental-1 <njengalewis133@gmail.com>
2025-11-22 06:17:53 +0300

    updating eslint and configuring package.json

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,36 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+import { defineConfig } from "eslint/config";
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
 
-module.exports = defineConfig([
-  expoConfig,
+const compat = new FlatCompat();
+
+export default defineConfig([
+  js.configs.recommended,
+
+  ...fixupConfigRules(compat.extends("expo")),
+
   {
-    ignores: ['dist/*'],
+    plugins: {
+      "react-hooks": fixupPluginRules(reactHooksPlugin),
+    },
+    rules: {
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
+
+  prettierConfig,
+
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
   },
 ]);
